fd94d5f2c3044ef306046b6cabbeabdad4eac9d8
Leonardo Janardi <leojanardi@gmail.com>
2020-06-17 19:29:12 -0300

    Ajuste app.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/Learn everything here/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Feature branch/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
 // test('renders learn react link', () => {
 //   const { getByText } = render(<App />);
